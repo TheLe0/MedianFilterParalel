591bcfbb05c1db1a22b5341b3f5913137445c6cb
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2020-12-14 22:56:50 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 #include <locale.h>
 #include <omp.h>
 
-#define N 5
-#define NTHREADS 5
+#define N 3
+#define NTHREADS 3
 
 typedef struct Image{
     int width;
@@ -70,199 +70,53 @@ void writeImage(FILE * fpo,char * image,IMAGE * IMG,unsigned char buffer[][3]){
     fclose(fpo);
 }
 
-/* void colorToGray(char * image,IMAGE * IMG,unsigned char buffer[][3]){
-    int i,y;
-    if(IMG->bitDepth <= 8){
-
-    }
-    else{
-        FILE * fpo = fopen(image,"wb");
-        fwrite(IMG->header,sizeof(unsigned char),54,fpo);
-        int size = IMG->height*IMG->width;
-        if(IMG->bitDepth <= 8)
-            fwrite(IMG->colorTable,sizeof(unsigned char),1024,fpo);
-        for(i=0;i<size;i++){
-            y = buffer[i][0]*0.3 + buffer[i][1]*0.59 + buffer[i][2]*0.11;
-            buffer[i][0] = buffer[i][1] = buffer[i][2] = y;
-            fputc(y,fpo);
-            fputc(y,fpo);
-            fputc(y,fpo);
-        }
-        fclose(fpo);
-    }
-} */
-
 int filterImage(char * image,IMAGE * IMG,unsigned char buffer[][3]){
     int x,y,i,j,k,n,sum[3],ch,z, id;
 
-    printf(" FILTER #1 : Blur\n FILTER #2 : Motion Blur\n FILTER #3 : Sharpen\nEnter choice : ");
-    scanf("%d",&ch);
+    n = N;
 
-    if (ch != 1 && ch != 2 && ch != 3)
-    {
+    int a[(n+2) * (n+2)][3];
+    float prod[3],b[(n+2) * (n+2)][3];
 
-        printf("OPCAO INVALIDA!");
-        return 0;
-    }
+    for(y=n/2;y<IMG->height-n/2;y++){
+        for(x=n/2;x<IMG->width-n/2;x++){
 
-    omp_set_num_threads(NTHREADS);
+            omp_set_num_threads(NTHREADS);
 
-    #pragma omp parallel private(id, n)
-    {
-        n = omp_get_num_threads();
-        id = omp_get_thread_num();
-
-        int a[n*n][3];
-        float prod[3],b[n*n][3];
-
-        if (ch == 1)
-        {
-            for (y=n/2;y<IMG->height-n/2;y++)
+            #pragma omp parallel private(id)
             {
-                for (x=n/2;x<IMG->width-n/2;x++)
-                {
-                    for (k=0;k<3;k++)
-                    {
-                        sum[k] = 0;
+                id = omp_get_thread_num();
+                sum[id] = 0;
+            }
+            
+            for(j=-n/2;j<=n/2;j++){
+                for(i=-n/2;i<=n/2;i++){
 
-                        for (j=-n/2;j<=n/2;j++)
-                        {
-                            for(i=-n/2;i<=n/2;i++)
-                            {
-                                for(k=0;k<3;k++)
-                                {
-                                    sum[k] += buffer[(y+j)*IMG->width + (x+i)][k];
-                                }
-                            }
-                        }
-                        for(k=0;k<3;k++)
-                        {
-                            buffer[y*IMG->width + x][k] = sum[k]/n/n;
-                        }
+                    omp_set_num_threads(NTHREADS);
+
+                    #pragma omp parallel private(id)
+                    {
+                        id = omp_get_thread_num();
+
+                        sum[id] += buffer[(y+j)*IMG->width + (x+i)][id];
                     }
                 }
+            }
+
+            omp_set_num_threads(NTHREADS);
+
+            #pragma omp parallel private(id)
+            {
+                id = omp_get_thread_num();
+                buffer[y*IMG->width + x][id] = sum[id]/n/n;
             }
         }
-        else if (ch == 2)
-        {
-            int p = 9;
-
-            double filter[9][9] =
-            {
-                1, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 1, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 1, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 1, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 1, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 1, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 1, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 1, 0,
-                0, 0, 0, 0, 0, 0, 0, 0, 1,
-            };
-            double factor = 1.0/9.0;
-            double bias = 0.0;
-
-            long w = IMG->height, h = IMG->width;
-            int filterX,filterY;
-
-            for (int x = 0; x < w; x++)
-            {
-                for(int y = 0; y < h; y++)
-                {
-
-                    double red = 0.0, green = 0.0, blue = 0.0;
-
-                    for (filterY = 0; filterY < p; filterY++)
-                    {
-                        for (filterX = 0; filterX < p; filterX++)
-                        {
-
-                            int imageX = (x - p/2 + filterX + w) % w;
-                            int imageY = (y - p/2 + filterY + h) % h;
-
-                            red += buffer[imageY * w + imageX][0] * filter[filterY][filterX];
-                            green += buffer[imageY * w + imageX][1] * filter[filterY][filterX];
-                            blue += buffer[imageY * w + imageX][2] * filter[filterY][filterX];
-                        }
-                        if(fabs(factor*red + bias)>255){
-                            buffer[y * w + x][0] = 255;
-                        }else{
-                            buffer[y * w + x][0] = fabs(factor*red + bias);
-                        }
-                        if(fabs(factor*green + bias)>255){
-                            buffer[y * w + x][1] = 255;
-                        }else{
-                            buffer[y * w + x][1] = fabs(factor*green + bias);
-                        }
-                        if(fabs(factor*blue + bias)>255){
-                            buffer[y * w + x][2] = 255;
-                        }else{
-                            buffer[y * w + x][2] = fabs(factor*blue + bias);
-                        }
-                    }
-                }
-            }
-        } 
-        else if(ch == 3)
-        {
-            int p = 5;
-
-            double filter[5][5] =
-            {
-                -1, -1, -1, -1, -1,
-                -1,  2,  2,  2, -1,
-                -1,  2,  8,  2, -1,
-                -1,  2,  2,  2, -1,
-                -1, -1, -1, -1, -1,
-            };
-            double factor = 1.0/8.0;
-            double bias = 0.0;
-
-            long w = IMG->height, h = IMG->width;
-            int filterX,filterY;
-
-            for(int x = 0; x < w; x++)
-            {
-                for(int y = 0; y < h; y++){
-
-                    double red = 0.0, green = 0.0, blue = 0.0;
-
-                    for(filterY = 0; filterY < p; filterY++)
-                    {
-                        for(filterX = 0; filterX < p; filterX++)
-                        {
-
-                            int imageX = (x - p/2 + filterX + w) % w;
-                            int imageY = (y - p/2 + filterY + h) % h;
-
-                            red += buffer[imageY * w + imageX][0] * filter[filterY][filterX];
-                            green += buffer[imageY * w + imageX][1] * filter[filterY][filterX];
-                            blue += buffer[imageY * w + imageX][2] * filter[filterY][filterX];
-                        }
-                        if(fabs(factor*red + bias)>255){
-                            buffer[y * w + x][0] = 255;
-                        }else{
-                            buffer[y * w + x][0] = fabs(factor*red + bias);
-                        }
-                        if(fabs(factor*green + bias)>255){
-                            buffer[y * w + x][1] = 255;
-                        }else{
-                            buffer[y * w + x][1] = fabs(factor*green + bias);
-                        }
-                        if(fabs(factor*blue + bias)>255){
-                            buffer[y * w + x][2] = 255;
-                        }else{
-                            buffer[y * w + x][2] = fabs(factor*blue + bias);
-                        }
-                    }
-                }
-            }
-        
-        }
+     
     }
     
     FILE * fpo;
     writeImage(fpo,image,IMG,buffer);
+
     return 1;
 }
 
@@ -298,30 +152,8 @@ int main(){
     FILE * fpo;
     writeImage(fpo,imageOut,&IMG,buffer);
 
-    int r,c,choice,factor,a,flag = 0;
-    char horv,* image;
+    (filterImage(imageOut,&IMG,buffer)) ? printf("\n...CONCLUIDO...\n") : printf("\n...ERRO...\n");
 
-    while(1){
-        printf("\n****** SELECIONE UMA OPCAO ******\n\n");
-        printf(" 1. Filtrar\n");
-        printf(" 0. SAIR\n");
-        int ch;
-        scanf("%d",&ch);
-        printf("\n******************************\n");
-        switch(ch){
-        case 1:
-            flag = filterImage(imageOut,&IMG,buffer);
-            break;
-        case 0:
-            exit(0);
-        default:
-            printf("\nINVALIDO !!");
-        }
-        if(flag)
-            printf("\n...CONCLUIDO...");
-        getchar();
-        flag = 0;
-        system("cls");
-    }
     return 0;
 }
+
